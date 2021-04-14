86bd2822dfa807b35cc725df6f971afb8d5ceeca
jmanly56 <jonathan.manly@wsu.edu>
2021-04-13 17:18:55 -0700

    Formatted code

--- util.c
+++ util.c
@@ -2,56 +2,55 @@
 
 int get_block(int dev, int blk, char *buf)
 {
-   lseek(dev, (long)blk*BLKSIZE, 0);
-   read(dev, buf, BLKSIZE);
-}   
+        lseek(dev, (long)blk * BLKSIZE, 0);
+        read(dev, buf, BLKSIZE);
+}
 int put_block(int dev, int blk, char *buf)
 {
-   lseek(dev, (long)blk*BLKSIZE, 0);
-   write(dev, buf, BLKSIZE);
-}   
+        lseek(dev, (long)blk * BLKSIZE, 0);
+        write(dev, buf, BLKSIZE);
+}
 
 int tokenize(char *pathname)
 {
-  // copy pathname into gpath[]; tokenize it into name[0] to name[n-1]
-  // Code in Chapter 11.7.2 
+        // copy pathname into gpath[]; tokenize it into name[0] to name[n-1]
+        // Code in Chapter 11.7.2
 }
-
 
 MINODE *iget(int dev, int ino)
 {
-  // return minode pointer of loaded INODE=(dev, ino)
-  // Code in Chapter 11.7.2
+        // return minode pointer of loaded INODE=(dev, ino)
+        // Code in Chapter 11.7.2
 }
 
 void iput(MINODE *mip)
 {
-  // dispose of minode pointed by mip
-  // Code in Chapter 11.7.2
-} 
+        // dispose of minode pointed by mip
+        // Code in Chapter 11.7.2
+}
 
 int search(MINODE *mip, char *name)
 {
-  // search for name in (DIRECT) data blocks of mip->INODE
-  // return its ino
-  // Code in Chapter 11.7.2
+        // search for name in (DIRECT) data blocks of mip->INODE
+        // return its ino
+        // Code in Chapter 11.7.2
 }
 
 int getino(char *pathname)
 {
-  // return ino of pathname
-  // Code in Chapter 11.7.2
+        // return ino of pathname
+        // Code in Chapter 11.7.2
 }
 
-int findmyname(MINODE *parent, u32 myino, char *myname) 
+int findmyname(MINODE *parent, u32 myino, char *myname)
 {
-  // WRITE YOUR code here:
-  // search parent's data block for myino;
-  // copy its name STRING to myname[ ];
+        // WRITE YOUR code here:
+        // search parent's data block for myino;
+        // copy its name STRING to myname[ ];
 }
 
 int findino(MINODE *mip, u32 *myino) // myino = ino of . return ino of ..
 {
-  // mip->a DIR minode. Write YOUR code to get mino=ino of .
-  //                                         return ino of ..
+        // mip->a DIR minode. Write YOUR code to get mino=ino of .
+        //                                         return ino of ..
 }
