c668a05e382cbbc9d15d240d0ebb122f635cb012
mchristianson5 <macychristianson5@gmail.com>
2021-04-13 18:26:51 -0700

    Added functions and fixed type.h

--- type.h
+++ type.h
@@ -1,48 +1,86 @@
 /*************** type.h file ************************/
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-
-typedef struct ext2_super_block SUPER;
+#ifndef TYPE_H
+#define TYPE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <linux/ext2_fs.h>
+#include <libgen.h>
+#include <string.h>
+#include <sys/stat.h>
+// define shorter TYPES for convenience
 typedef struct ext2_group_desc GD;
+typedef struct ext2_super_block SUPER;
 typedef struct ext2_inode INODE;
 typedef struct ext2_dir_entry_2 DIR;
-
-SUPER *sp;
-GD *gp;
-INODE *ip;
-DIR *dp;
-
-#define FREE 0
-#define READY 1
-
 #define BLKSIZE 1024
-#define NMINODE 128
-#define NFD 16
+// Block number of EXT2 FS on FD
+#define SUPERBLOCK 1
+#define GDBLOCK 2
+#define ROOT_INODE 2
+// Default dir and regular file modes
+#define DIR_MODE 0x41ED
+#define FILE_MODE 0x81AE
+#define SUPER_MAGIC 0xEF53
+#define SUPER_USER 0
+// Proc status
+#define FREE 0
+#define BUSY 1
+// file system table sizes
+#define NMINODE 100
+#define NMTABLE 10
 #define NPROC 2
+#define NFD 10
+#define NOFT 40
+// Open File Table
+typedef struct oft{
+int mode;
+int refCount;
+struct minode *minodePtr;
+int offset;
+}OFT;
+// PROC structure
+typedef struct proc{
+struct Proc *next;
+int pid;
+int uid;
+int gid;
+320 11 EXT2 File System
+int ppid;
+int status;
+struct minode *cwd;
+OFT *fd[NFD];
+}PROC;
+// In-memory inodes structure
+typedef struct minode{
+INODE INODE; // disk inode
+int dev, ino;
+int refCount; // use count
+int dirty; // modified flag
+int mounted; // mounted flag
+struct mount *mntPtr; // mount table pointer
+// int lock; // ignored for simple FS
+}MINODE;
+// Open file Table // opened file instance
+typedef struct oft{
+int mode; // mode of opened file
+int refCount; // number of PROCs sharing this instance
+MINODE *minodePtr; // pointer to minode of file
+int offset; // byte offset for R|W
+}OFT;
+// Mount Table structure
+typedef struct mtable{
+int dev; // device number; 0 for FREE
+int ninodes; // from superblock
+int nblocks;
+int free_blocks // from superblock and GD
+int free_inodes
+int bmap; // from group descriptor
+int imap;
+int iblock; // inodes start block
+MINODE *mntDirPtr; // mount point DIR pointer
+char devName[64]; //device name
+char mntName[64]; // mount point DIR name
+}MTABLE;
 
-typedef struct minode {
-        INODE INODE;
-        int dev, ino;
-        int refCount;
-        int dirty;
-        int mounted;
-        struct mntable *mptr;
-} MINODE;
-
-typedef struct oft {
-        int mode;
-        int refCount;
-        MINODE *mptr;
-        int offset;
-} OFT;
-
-typedef struct proc {
-        struct proc *next;
-        int pid;
-        int ppid;
-        int status;
-        int uid, gid;
-        MINODE *cwd;
-        OFT *fd[NFD];
-} PROC;
+#endif
--- util.c
+++ util.c
@@ -1,56 +1,179 @@
 /*********** util.c file ****************/
 
-int get_block(int dev, int blk, char *buf)
-{
-        lseek(dev, (long)blk * BLKSIZE, 0);
-        read(dev, buf, BLKSIZE);
+#include <ext2fs/ext2_fs.h>
+#include <fctnl.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include "type.h"
+
+MINODE minode[NMINODE];
+MINODE *root;
+PROC proc[NPROC], *running;
+MOUNT mtable[NMTABLE];
+char gpath[256];
+char *name[64]; // token string pointers
+int n, fd, dev;
+char line[256];
+char cmd[64];
+char pathname[256];
+char cwd[256];
+char gline[256]; // holds token strings, each pointed by a name[i]
+int nname;       // number of token strings
+
+int get_block(int dev, int blk, char *buf) {
+  lseek(dev, (long)blk * BLKSIZE, 0);
+  read(dev, buf, BLKSIZE);
 }
-int put_block(int dev, int blk, char *buf)
-{
-        lseek(dev, (long)blk * BLKSIZE, 0);
-        write(dev, buf, BLKSIZE);
+int put_block(int dev, int blk, char *buf) {
+  lseek(dev, (long)blk * BLKSIZE, 0);
+  write(dev, buf, BLKSIZE);
 }
 
-int tokenize(char *pathname)
-{
-        // copy pathname into gpath[]; tokenize it into name[0] to name[n-1]
-        // Code in Chapter 11.7.2
+int tokenize(char *pathname) {
+  // copy pathname into gpath[]; tokenize it into name[0] to name[n-1]
+  // Code in Chapter 11.7.2
+  char *s;
+  strcpy(gline, pathname);
+  nname = 0;
+  s = strtok(gline, "/");
+  while (s) {
+    name[nname++] = s;
+    s = strtok(0, "/");
+  }
 }
 
-MINODE *iget(int dev, int ino)
-{
-        // return minode pointer of loaded INODE=(dev, ino)
-        // Code in Chapter 11.7.2
+MINODE *iget(int dev, int ino) {
+  // return minode pointer of loaded INODE=(dev, ino)
+  // Code in Chapter 11.7.2
+  MINODE *mip;
+  MTABLE *mp;
+  INODE *ip;
+  int i, block, offset;
+  char buf[BLKSIZE];
+  // serach in-memory minodes first
+  for (i = 0; i < NMINODES; i++) {
+    mip = &MINODE[i];
+    if (mip->refCount && (mip->dev == dev) && (mip->ino == ino)) {
+      mip->refCount++;
+      return mip;
+    }
+  }
+
+  // needed INODE=(dev,ino) not in memory
+  mip = mialloc(); // allocate a FREE minode
+  mip->dev = dev;
+  mip->ino = ino;                 // assign to (dev, ino)
+  block = (ino - 1) / 8 + iblock; // disk block containing this inode
+  offset = (ino - 1) % 8;         // which inode in this block
+  get_block(dev, block, buf);
+  ip = (INODE *)buf + offset;
+  mip->INODE = *ip; // copy inode to minode.INODE
+  // initialize minode
+  mip->refCount = 1;
+  mip->mounted = 0;
+  mip->dirty = 0;
+  mip->mountptr = 0;
+  printf("Inside iget: ino: %d, block: %d, offset:%d\n", ino, block, offset);
+  return mip;
 }
 
-void iput(MINODE *mip)
-{
-        // dispose of minode pointed by mip
-        // Code in Chapter 11.7.2
+void iput(MINODE *mip) {
+  // dispose of minode pointed by mip
+  // Code in Chapter 11.7.2
+  INODE *ip;
+  int i, block, offset;
+  char buf[BLKSIZE];
+  if (mip == 0)
+    return;
+  mip->refCount--; // dec refCount by 1
+  if (mip->refCount > 0)
+    return; // still has user
+  if (mip->dirty == 0)
+    return; // no need to write back
+  // write INODE back to disk
+  block = (mip->ino - 1) / 8 + iblock;
+  offset = (mip->ino - 1) % 8;
+  // get block containing this inode
+  get_block(mip->dev, block, buf);
+  ip = (INODE *)buf + offset;      // ip points at INODE
+  *ip = mip->INODE;                // copy INODE to inode in block
+  put_block(mip->dev, block, buf); // write back to disk
+  midalloc(mip);                   // mip->refCount = 0;
 }
 
-int search(MINODE *mip, char *name)
-{
-        // search for name in (DIRECT) data blocks of mip->INODE
-        // return its ino
-        // Code in Chapter 11.7.2
+int search(MINODE *mip, char *name) {
+  // search for name in (DIRECT) data blocks of mip->INODE
+  // return its ino
+  // Code in Chapter 11.7.2
+  int i;
+  char *cp, temp[256], sbuf[BLKSIZE];
+  DIR *dp;
+  for (i = 0; i < 12; i++) { // search DIR direct blocks only
+    if (mip->INODE.i_block[i] == 0)
+      return 0;
+    get_block(mip->dev, mip->INODE.i_block[i], sbuf);
+    dp = (DIR *)sbuf;
+    cp = sbuf;
+    while (cp < sbuf + BLKSIZE) {
+      strncpy(temp, dp->name, dp->name_len);
+      temp[dp->name_len] = 0;
+      printf("%8d%8d%8u %s\n", dp->inode, dp->rec_len, dp->name_len, temp);
+      if (strcmp(name, temp) == 0) {
+        printf("found %s : inumber = %d\n", name, dp->inode);
+        return dp->inode;
+      }
+      cp += dp->rec_len;
+      dp = (DIR *)cp;
+    }
+  }
+  return 0;
 }
 
-int getino(char *pathname)
-{
-        // return ino of pathname
-        // Code in Chapter 11.7.2
+int getino(char *pathname) {
+  // return ino of pathname
+  // Code in Chapter 11.7.2
+  MINODE *mip;
+  int i, ino;
+  if (strcmp(pathname, "/") == 0) {
+    return 2; // return root ino=2
+  }
+  if (pathname[0] == ’/’)
+    mip = root; // if absolute pathname: start from root
+  else
+    mip = running->cwd;                // if relative pathname: start from CWD
+  mip->refCount++;                     // in order to iput(mip) later
+  tokenize(pathname);                  // assume: name[ ], nname are globals
+  for (i = 0; i < nname; i++) {        // search for each component string
+    if (!S_ISDIR(mip->INODE.i_mode)) { // check DIR type
+      printf("%s is not a directory\n", name[i]);
+      iput(mip);
+      return 0;
+    }
+    ino = search(mip, name[i]);
+    if (!ino) {
+      printf("no such component name %s\n", name[i]);
+      iput(mip);
+      return 0;
+    }
+    iput(mip);            // release current minode
+    mip = iget(dev, ino); // switch to new minode
+  }
+  iput(mip);
+  return ino;
 }
 
-int findmyname(MINODE *parent, u32 myino, char *myname)
-{
-        // WRITE YOUR code here:
-        // search parent's data block for myino;
-        // copy its name STRING to myname[ ];
+int findmyname(MINODE *parent, u32 myino, char *myname) {
+  // WRITE YOUR code here:
+  // search parent's data block for myino;
+  // copy its name STRING to myname[ ];
+
 }
 
 int findino(MINODE *mip, u32 *myino) // myino = ino of . return ino of ..
 {
-        // mip->a DIR minode. Write YOUR code to get mino=ino of .
-        //                                         return ino of ..
+  // mip->a DIR minode. Write YOUR code to get mino=ino of .
+  //                                         return ino of ..
 }
